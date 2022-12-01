461a6d0cd8619b430959403c499f948edf6a5aac
Vitor A. Rinkawetsky <88173262+NerdskyX@users.noreply.github.com>
2022-12-01 19:46:55 -0300

    alteraç~eos em entity emprestimo

--- src/com/ifsc/tds/Vitor/Cassio/Igor/entity/Emprestimo.java
+++ src/com/ifsc/tds/Vitor/Cassio/Igor/entity/Emprestimo.java
@@ -1,5 +1,68 @@
 package com.ifsc.tds.Vitor.Cassio.Igor.entity;
 
-public class Emprestimo {
+import java.sql.Date;
+import java.time.format.DateTimeFormatter;
 
+public class Emprestimo {
+	private Long id;
+	private Date data_esmprestimo;
+	private Date data_entrega;
+	private String obs;
+	private String valorEmprestimo;
+	private Cliente cliente;
+	private Filmes filmes;
+	private Date ano;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Date getData_esmprestimo() {
+		return data_esmprestimo;
+	}
+	public void setData_esmprestimo(Date data_esmprestimo) {
+		this.data_esmprestimo = data_esmprestimo;
+	}
+	public Date getData_entrega() {
+		return data_entrega;
+	}
+	public void setData_entrega(Date data_entrega) {
+		this.data_entrega = data_entrega;
+	}
+	public String getObs() {
+		return obs;
+	}
+	public void setObs(String obs) {
+		this.obs = obs;
+	}
+	public String getValorEmprestimo() {
+		return valorEmprestimo;
+	}
+	public void setValorEmprestimo(String valorEmprestimo) {
+		this.valorEmprestimo = valorEmprestimo;
+	}
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	public Filmes getFilmes() {
+		return filmes;
+	}
+	public void setFilmes(Filmes filmes) {
+		this.filmes = filmes;
+	}
+	public Date getAno() {
+		return ano;
+	}
+	public void setAno(Date ano) {
+		this.ano = ano;
+	}
+	public String getAnoFormatado() {
+		DateTimeFormatter dataFormatada = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		return this.ano.toLocalDate().format(dataFormatada).toString();
+	}
 }
