96884a89d7944639245a759ff869cfdf1221aed4
Bundinha Molhada <116108735+MamedsonPereira@users.noreply.github.com>
2022-12-01 18:53:20 -0300

    Tela e controller Cadastro

---
+++ src/com/ifst/tds/Vitor/Cassio/Igor/controller/CadastroListaController.java
@@ -0,0 +1,54 @@
+package com.ifst.tds.Vitor.Cassio.Igor.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class CadastroListaController {
+
+    @FXML
+    private Button btnEditar;
+
+    @FXML
+    private Button btnExcluir;
+
+    @FXML
+    private Button btnIncluir;
+
+    @FXML
+    private Label lblData;
+
+    @FXML
+    private Label lblEmail;
+
+    @FXML
+    private Label lblNome;
+
+    @FXML
+    private TableColumn<?, ?> tbcCodigo;
+
+    @FXML
+    private TableColumn<?, ?> tbcNome;
+
+    @FXML
+    private TableView<?> tbvCadastro;
+
+    @FXML
+    void onClickBtnEditar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickBtnExcluir(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickBtnIncluir(ActionEvent event) {
+
+    }
+
+}
--- src/com/ifst/tds/Vitor/Cassio/Igor/controller/MenuController.java
+++ src/com/ifst/tds/Vitor/Cassio/Igor/controller/MenuController.java
@@ -1,5 +1,84 @@
 package com.ifst.tds.Vitor.Cassio.Igor.controller;
 
+import java.awt.Menu;
+import java.awt.MenuBar;
+import java.awt.MenuItem;
+import java.awt.event.ActionEvent;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 public class MenuController {
+
+    @FXML
+    private Menu menuAjuda;
+
+    @FXML
+    private MenuBar menuBar;
+
+    @FXML
+    private Menu menuCadastro;
+
+    @FXML
+    private MenuItem mnClientes;
+
+    @FXML
+    private MenuItem mnEmprestimos;
+
+    @FXML
+    private MenuItem mnFilmes;
+
+    @FXML
+    private MenuItem mnFilmes1;
+
+    @FXML
+    private MenuItem mnSair;
+
+    @FXML
+    private MenuItem mnSobre;
+
+    @FXML
+    private VBox pnlPrincipal;
+    
+    private Stage stage;
+
+	public Stage getStage() {
+		return stage;
+	}
+
+	public void setStage(Stage stage) {
+		this.stage = stage;
+	}
+
+    @FXML
+    void onClickMnClientes(ActionEvent event) {
+    	
+    }
+
+    @FXML
+    void onClickMnEmprestimos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickMnF(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickMnFilmes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickMnSair(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickMnSobre(ActionEvent event) {
+
+    }
 
 }
