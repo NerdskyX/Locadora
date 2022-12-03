42e866d03525419286cc848facc73ed06cb0d99e
Bundinha Molhada <116108735+MamedsonPereira@users.noreply.github.com>
2022-12-03 19:13:51 -0300

    add telas e controllers

---
+++ src/com/ifst/tds/Vitor/Cassio/Igor/controller/FilmeAddController.java
@@ -0,0 +1,32 @@
+package com.ifst.tds.Vitor.Cassio.Igor.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class FilmeAddController {
+
+    @FXML
+    private Button btnCancelar;
+
+    @FXML
+    private Button btnConfirmar;
+
+    @FXML
+    private TextField txtLancamento;
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    void onClickBtnCancelar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickBtnConfirmar(ActionEvent event) {
+
+    }
+
+}
