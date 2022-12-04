4fb9d4a6603d46bb9b29d954772f9deb127531d1
Vitor A. Rinkawetsky <88173262+NerdskyX@users.noreply.github.com>
2022-12-03 21:29:05 -0300

    Update Menu.java

--- src/com/ifsc/tds/Vitor/Cassio/Igor/menu/Menu.java
+++ src/com/ifsc/tds/Vitor/Cassio/Igor/menu/Menu.java
@@ -1,11 +1,10 @@
 package com.ifsc.tds.Vitor.Cassio.Igor.menu;
 
-
-import javafx.geometry.Rectangle2D;
 import com.ifst.tds.Vitor.Cassio.Igor.controller.MenuController;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Modality;
@@ -13,46 +12,45 @@ import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Menu extends Application {
-		@Override
-		public void start(Stage primaryStage) {
-			try {
-				// carregando o loader
-				FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/ifsc/tds/Vitor/Cassio/Igor/view/Menu.fxml"));
-				Parent menuXML = loader.load();
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			// carregando o loader
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/ifsc/tds/Vitor/Cassio/Igore/view/Menu.fxml"));
+			Parent menuXML = loader.load();
 
-				// carregando o controller e a scene
-				MenuController menuController = loader.getController();
-				Scene menuLayout = new Scene(menuXML);
+			// carregando o controller e a scene
+			MenuController menuController = loader.getController();
+			Scene menuLayout = new Scene(menuXML);
 
-				Stage menuJanela = new Stage();
-				menuJanela.initModality(Modality.APPLICATION_MODAL);
-				menuJanela.resizableProperty().setValue(Boolean.FALSE);
-				menuJanela.setScene(menuLayout);
-				menuJanela.setTitle("Menu do sistema");
+			Stage menuJanela = new Stage();
+			menuJanela.initModality(Modality.APPLICATION_MODAL);
+			menuJanela.resizableProperty().setValue(Boolean.FALSE);
+			menuJanela.setScene(menuLayout);
+			menuJanela.setTitle("Menu do sistema");
 
-				// atribuindo evento para fechar janela
-				menuJanela.setOnCloseRequest(e -> {
-					if (menuController.onCloseQuery()) {
-						System.exit(0);
-					} else {
-						e.consume();
-					}
-				});
+			// atribuindo evento para fechar janela
+			menuJanela.setOnCloseRequest(e -> {
+				if (menuController.onCloseQuery()) {
+					System.exit(0);
+				} else {
+					e.consume();
+				}
+			});
 
-				menuJanela.show();
+			menuJanela.show();
 
-				// posicionando a janela no centro da tela do computador
-				Rectangle2D posicaoJanela = Screen.getPrimary().getVisualBounds();
-				menuJanela.setX((posicaoJanela.getWidth() - menuJanela.getWidth()) / 2);
-				menuJanela.setY((posicaoJanela.getHeight() - menuJanela.getHeight()) / 2);
+			// posicionando a janela no centro da tela do computador
+			Rectangle2D posicaoJanela = Screen.getPrimary().getVisualBounds();
+			menuJanela.setX((posicaoJanela.getWidth() - menuJanela.getWidth()) / 2);
+			menuJanela.setY((posicaoJanela.getHeight() - menuJanela.getHeight()) / 2);
 
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-
-		public static void main(String[] args) {
-			launch(args);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
-	
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
