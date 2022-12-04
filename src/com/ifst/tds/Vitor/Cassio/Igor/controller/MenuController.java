979916b4a09e667205a8d6b2da77880024c8e413
Bundinha Molhada <116108735+MamedsonPereira@users.noreply.github.com>
2022-12-04 00:29:21 -0300

    arrumei o menuController

--- src/com/ifst/tds/Vitor/Cassio/Igor/controller/MenuController.java
+++ src/com/ifst/tds/Vitor/Cassio/Igor/controller/MenuController.java
@@ -1,13 +1,13 @@
 package com.ifst.tds.Vitor.Cassio.Igor.controller;
 
+import java.net.URL;
 import java.util.Optional;
-
-import com.peregrinoti.controller.CaixaListaController;
-import com.peregrinoti.controller.TipoColecaoListaController;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
@@ -17,8 +17,9 @@ import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.Modality;
 
-public class MenuController {
+public class MenuController implements Initializable {
 
     @FXML
     private Menu menuAjuda;
@@ -61,19 +62,19 @@ public class MenuController {
     void onClickMnClientes(ActionEvent event) {
     	try {
 			// carregando o loader
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/ifsc/tds/Vitor/Cassio/Igor/view/CadastroListaController.fxml"));
-			Parent CadastroListaXML = loader.load();
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/ifsc/tds/Vitor/Cassio/Igor/view/CadastroLista.fxml"));
+			Parent cadastroListaXML = loader.load();
 
 			// carregando o controller e a scene
-			CadastroListaController CadastroListaController = loader.getController();
-			Scene CadastroListaLayout = new Scene(CadastroListaXML);
+			CadastroListaController cadastroListaController = loader.getController();
+			Scene cadastroListaLayout = new Scene(cadastroListaXML);
 
-			this.getStage().setScene(CadastroListaLayout);
-			this.getStage().setTitle("Cadastro de Cliente");
+			this.getStage().setScene(cadastroListaLayout);
+			this.getStage().setTitle("Cadastro do cliente");
 
 			// atribuindo evento para fechar janela
 			this.getStage().setOnCloseRequest(e -> {
-				if (CadastroListaController.onCloseQuery()) {
+				if (cadastroListaController.onCloseQuery()) {
 					this.getStage().close();
 				} else {
 					e.consume();
@@ -84,7 +85,8 @@ public class MenuController {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-    }
+	}
+
 
     @FXML
     void onClickMnEmprestimos(ActionEvent event) {
@@ -160,6 +162,11 @@ public class MenuController {
 		alerta.showAndWait();
     }
     
+    @Override
+	public void initialize(URL location, ResourceBundle resources) {
+    	this.configuraStage();
+	}
+    
     public boolean onCloseQuery() {
 		Alert alerta = new Alert(Alert.AlertType.CONFIRMATION);
 		alerta.setTitle("Pergunta");
@@ -169,6 +176,12 @@ public class MenuController {
 		alerta.getButtonTypes().setAll(botaoSim, botaoNao);
 		Optional<ButtonType> resultado = alerta.showAndWait();
 		return resultado.get() == botaoSim ? true : false;
+	}
+    
+    public void configuraStage() {
+		this.setStage(new Stage());
+		this.getStage().initModality(Modality.APPLICATION_MODAL);
+		this.getStage().resizableProperty().setValue(Boolean.FALSE);
 	}
 
 }
